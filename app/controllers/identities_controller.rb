5df917cb7c8899e489bd204f6dc03d83be306aeb
Brian Pattison <brian@brianpattison.com>
2012-01-04 00:39:34 -0600

    Add user management

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -1,5 +1,21 @@
 class IdentitiesController < ApplicationController
+  before_filter :authenticate
+  
   def new
     @identity = env['omniauth.identity']
+  end
+  
+  def index
+    @identities = Identity.all
+  end
+  
+  def destroy
+    @identity = Identity.find(params[:id])
+    @identity.destroy
+
+    respond_to do |format|
+      format.html { redirect_to identities_url }
+      format.json { head :ok }
+    end
   end
 end
