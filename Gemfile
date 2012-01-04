023c167b3351c912af68400e6a5623935b1aa8e9
Brian Pattison <brian@brianpattison.com>
2012-01-03 19:24:19 -0600

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+gem 'pg'
+
+# Gems used only for assets and not required in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'jquery-rails'
+gem 'omniauth-identity'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ingredients::Application.config.secret_token = 'b2b4dba7319a5ff080f74719fc3e0f663ce29c00edfa74aabd1bd76138a5b7fb7149c90951b858df940de251dd42cf71886a37384a67cd2819cee4aad347c0e9'
